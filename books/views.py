0e29598dd1663b0dddbf199564c6fe3de97e845e
Tim Bennett <bennett.tim@gmail.com>
2016-12-23 13:39:05 +1100

    still working on books app

--- books/views.py
+++ books/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from books.models import Book, Chapter_Notes
+from django.shortcuts import render_to_response, get_object_or_404
 
-# Create your views here.
+def index(request):
+    return render_to_response('index.html', {
+        'books': Books.objects.all(),
+        'posts': Chapter_Notes.objects.all(),
+    })
+
+def view_post(request, slug):
